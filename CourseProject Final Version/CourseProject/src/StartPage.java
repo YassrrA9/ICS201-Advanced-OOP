ed41b1532aa0c470223c465e1c5bdde2f65d906f
Yasser Ali <94631183+YassrrA9@users.noreply.github.com>
2022-02-24 20:34:25 +0300

    Cards Game with a group

---
+++ CourseProject Final Version/CourseProject/src/StartPage.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import views.ResultsPage;
+import views.game.AlertBox;
+import views.game.GamePane;
+import views.game.HomePane;
+
+public class StartPage extends Application {
+    private static final double PREF_HEIGHT = 700;
+    private static final double PREF_WIDTH = 600;
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Memory Puzzle");
+        stage.setScene(new Scene( HomePane.getHomePane(stage),300, 300));
+        stage.setHeight(PREF_HEIGHT);
+        stage.setWidth(PREF_WIDTH);
+        stage.show();
+    }
+
+
+}
